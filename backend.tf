8833706f52c443ceca710cfecf3682ac6d5dd1db
Anil Bidary <57112052+anilbd2003@users.noreply.github.com>
2023-03-17 00:31:54 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,8 @@
 terraform {
     backend "s3" {
-        bucket = "terraform-state-lock-bucket-2023"
+        bucket = "terraformbackendbucket2023"
         key    = "backend/s3/terraform.tfstate"
         region     = "us-east-1"
-        profile = "custom"
-       dynamodb_table  = "dynamodb-state-locking"
-    }
+       }
 } 
 

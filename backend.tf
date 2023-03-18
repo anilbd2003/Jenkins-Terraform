4c57cca0312706a38fdbca7dc30e8f0f19cdbf3a
Anil Bidary <57112052+anilbd2003@users.noreply.github.com>
2023-03-18 11:58:29 -0500

    Add files via upload

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+    backend "s3" {
+        bucket = "terraformbackendbucket2023"
+        key    = "backend/s3/terraform.tfstate"
+        region     = "us-east-1"
+        dynamodb_table  = "dynamodb-state-locking"
+       
+    }
+} 

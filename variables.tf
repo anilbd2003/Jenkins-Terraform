d02ca7d4be3bf260c10d186b5f13af1e0ec2a663
Anil Bidary <57112052+anilbd2003@users.noreply.github.com>
2023-03-19 14:46:43 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,5 +13,5 @@ variable "instance_type" {
 variable "name_tag" {
    type        = string
    description = "Name of the EC2 instance"
-   default     = "Ubuntu server"
+   default     = "My Ubuntu server"
 }
